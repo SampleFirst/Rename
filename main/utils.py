b6177df03c84f6e666c1318c4f7156db657e9512
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-03-03 20:07:21 +0530

    Update utils.py

--- main/utils.py
+++ main/utils.py
@@ -1,4 +1,4 @@
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.types import *
 import math
 import os
 import time
@@ -15,21 +15,22 @@ async def progress_message(current, total, ud_type, message, start):
         time_to_completion = round((total - current) / speed) * 1000
         estimated_total_time = elapsed_time + time_to_completion
         elapsed_time = TimeFormatter(milliseconds=elapsed_time)
-        estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
+        estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)                                    
         progress = "\n{0}{1}".format(
             ''.join(["⬢" for i in range(math.floor(percentage / 5))]),
-            ''.join(["⬡" for i in range(20 - math.floor(percentage / 5))]))
+            ''.join(["⬡" for i in range(20 - math.floor(percentage / 5))]))                                  
         tmp = progress + PROGRESS_BAR.format(
             a=round(percentage, 2),
             b=humanbytes(current),
             c=humanbytes(total),
             d=humanbytes(speed),
-            f=estimated_total_time if estimated_total_time != '' else "0 s")
+            f=estimated_total_time if estimated_total_time != '' else "0 s")                               
         try:
             chance = [[InlineKeyboardButton("🚫 Cancel", callback_data="del")]]
-            await message.edit(text="{}\n{}".format(ud_type, tmp), reply_markup=InlineKeyboardMarkup(chance))
-        except Exception as e:
-            print("An error occurred while editing message:", e)
+            await message.edit(text="{}\n{}".format(ud_type, tmp), reply_markup=InlineKeyboardMarkup(chance))         
+        except:
+            pass
+
 
 def humanbytes(size):
     units = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB"]
@@ -39,6 +40,7 @@ def humanbytes(size):
         i += 1
         size /= 1024.0
     return "%.2f %s" % (size, units[i])
+
 
 def TimeFormatter(milliseconds: int) -> str:
     seconds, milliseconds = divmod(int(milliseconds), 1000)
@@ -51,3 +53,6 @@ def TimeFormatter(milliseconds: int) -> str:
           ((str(seconds) + "s, ") if seconds else "") + \
           ((str(milliseconds) + "ms, ") if milliseconds else "")
     return tmp[:-2]
+
+
+
